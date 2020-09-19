6d720b0e6283adc7e1e01ea82b5053d116dff39a
qister <qistersick@gmail.com>
2020-09-19 14:18:49 +0300

    на сервер добавлен тайпскрипт

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,41 +1,39 @@
-const WebSocket = require('ws')
+import WebSoc from 'ws'
 
-const server = new WebSocket.Server({ port: 8000 })
-const businessServer = new WebSocket.Server({ port: 8001 })
+const server = new WebSoc.Server({ port: 8000 })
+const businessServer = new WebSoc.Server({ port: 8001 })
 
 const clients = new Set()
 const businessClients = new Set()
 
-server.on('connection', (ws, request, client) => {
-  // console.log(ws);
-  // console.log(request)
+server.on('connection', (ws: any) => {
   clients.add(ws)
   let id = 1
-  ws.on('message', (message) => {
+  ws.on('message', (message: any) => {
     if (message === 'exit') {
-      ws.close()
+      ws.close() 
     } else {
       const parsedMessage = JSON.parse(message)
       const newMessage = {...parsedMessage, data: {...parsedMessage.data, id}}
       id++
       console.log(newMessage)
       const nextMessage = JSON.stringify(newMessage)
-      businessClients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) client.send(nextMessage)
+      businessClients.forEach((client: any) => {
+        if (client.readyState === WebSoc.OPEN) client.send(nextMessage)
       })
     }
   })
   ws.send('Hello', null, () => console.log('Client connected'))
 })
 
-businessServer.on('connection', (ws, request, client) => {
+businessServer.on('connection', (ws: any) => {
   businessClients.add(ws)
-  ws.on('message', (message) => {
+  ws.on('message', (message: any) => {
     if (message === 'exit') {
       ws.close()
     } else {
-      clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
+      clients.forEach((client: any) => {
+        if (client.readyState === WebSoc.OPEN) {
           console.log(message)
           client.send(message)
         }
